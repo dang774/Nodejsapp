aeec6f451e00b0103c2a576681562085f050f9ec
Daniel Grant <dankhd1080p@gmail.com>
2022-03-14 14:07:57 +1100

    Second commit

--- index.js
+++ index.js
@@ -8,8 +8,18 @@ const app = express();
 
 //Route to main directory will return a message to the user 'Hi Dan'
 app.get('/', (req, res) => {
-  res.send('Hi Dan. This is new.')
+  res.send('Hi Dan. This is your REST API with Nodejs and Express.')
 });
+
+
+app.get('/api/courses', (req, res) => {
+  res.send([1,2,3])
+});
+
+// Parameter create for the ID 
+app.get('/api/courses/:id', (req, res) => {
+  res.send(req.params.id);
+})
 
 
 //Creates a listener on port 8080
